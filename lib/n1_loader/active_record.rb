1b7d1655ceb81d139bfd04df3b5e83c3eaccb237
djezzl <lawliet.djez@gmail.com>
2021-12-26 12:41:28 +0200

    Fixes

--- lib/n1_loader/active_record.rb
+++ lib/n1_loader/active_record.rb
@@ -4,8 +4,11 @@ require_relative "../n1_loader"
 
 require "active_record"
 
-require_relative "active_record/associations_preloader"
-
 ActiveSupport.on_load(:active_record) do
+  if ActiveRecord::VERSION::MAJOR < 6
+    require_relative "active_record/associations_preloader_v5"
+  else
+    require_relative "active_record/associations_preloader"
+  end
   ActiveRecord::Associations::Preloader.prepend(N1Loader::ActiveRecord::Associations::Preloader)
 end
